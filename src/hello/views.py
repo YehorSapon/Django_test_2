936429b01966a5836bb29b4a6bd26563bd5a9de4
Yehor Sapon <egorsapon@gmail.com>
2022-12-09 22:07:05 +0100

    first hello pages

--- src/hello/views.py
+++ src/hello/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.utils.timezone import datetime
+from django.http import HttpResponse
 
-# Create your views here.
+def home(request):
+    return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hello/hello.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        },
+    )
--- src/proj/settings.py
+++ src/proj/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # my apps
+    'hello'
 ]
 
 MIDDLEWARE = [
@@ -83,21 +85,21 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-
-AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
-]
+if DEBUG == False:
+    AUTH_PASSWORD_VALIDATORS = [
+        {
+            'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        },
+        {
+            'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        },
+    ]
 
 
 # Internationalization
