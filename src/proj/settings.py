fe4cc79a81dd78012f5d6ec35850064b65126d2a
Yehor Sapon <egorsapon@gmail.com>
2022-12-09 22:25:10 +0100

    prepare to work with static

--- src/proj/settings.py
+++ src/proj/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
-if DEBUG == False:
+if DEBUG != True:
     AUTH_PASSWORD_VALIDATORS = [
         {
             'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -118,6 +118,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'static_collected'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media_collected'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
