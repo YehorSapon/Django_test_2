970fd8a2270ab88eaadb503019c7b8c3e1cdc55f
Yehor Sapon <egorsapon@gmail.com>
2022-12-15 22:36:59 +0100

    new index

--- src/authentification/urls.py
+++ src/authentification/urls.py
@@ -1,4 +1,4 @@
-"""proj URL Configuration
+"""authentification URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,14 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+app_name = 'authentification'
+from django.urls import path
+from authentification import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("hello.urls")),
-    path('shop/', include('shop.urls'))
+    path("", views.home, name="login"),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
--- src/shop/urls.py
+++ src/shop/urls.py
@@ -1,4 +1,4 @@
-"""proj URL Configuration
+"""shop URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -13,14 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from shop import views
+from django.urls import path
+app_name = 'shop'
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path("", include("hello.urls")),
-    path('shop/', include('shop.urls'))
+    path("", views.index, name="index"),
 ]
-
-urlpatterns += staticfiles_urlpatterns()
